59e00f8f9c4e24c35106b5406834de4738aaf9b6
yanoom <yanoom@gmail.com>
2018-07-17 19:16:48 +0300

    Added menu loop. Print a task by ObjId

--- s.py
+++ s.py
@@ -3,12 +3,16 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import pprint
 
-def print_menu():
+def print_menu(print_welcome=False):
    "Prints menu"
-   print("Welcome to scheduplan beta0.1")
-   return
+   if(print_welcome):
+        print("Welcome to scheduplan beta0.1")
+   print("1- insert new task")
+   print("2- print task by id")
+   print("3- print all tasks")
+   print("Q- Quit")
 
-def read_input(type, prompt):
+def read_input(prompt, type="string"):
     if ("int" == type):
         try:
             mode=int(input(prompt))
@@ -31,20 +35,21 @@ def print_task_by_obj(task_obj):
     pprint.pprint(db.tasks.find_one({'_id': task_obj}))
 
 db = db_init()
-print_menu()
-print("1- insert new task")
-print("2- print task by id")
-print("3- print all tasks")
-selection = read_input("int", "Please select an option:")
-print("selection = ", str(selection))
-if (1 == selection):
-    new_task = {"name":"new task"}
-    new_task_id = db.tasks.insert_one(new_task).inserted_id
-    print_task_by_obj(new_task_id)
-if (2 == selection):
-    print_task_by_obj(ObjectId('5b492a337906a400e4280ea3'))
-if (3 == selection):
-    print("*** All tasks ***\n*** ***")
-    for task in db.tasks.find():
-        pprint.pprint(task)
-        print("------")
+print_menu(True)
+selection = 0
+while (('q' != selection) and ('Q' != selection)):
+    print_menu()
+    selection = read_input("Please select an option:")
+    print("selection = ", selection)
+    if ('1' == selection):
+        new_task = {"name":"new task"}
+        new_task_id = db.tasks.insert_one(new_task).inserted_id
+        print_task_by_obj(new_task_id)
+    if ('2' == selection):
+        obj_id = read_input("Please insert ObjectId: ")
+        print_task_by_obj(ObjectId(obj_id))
+    if ('3' == selection):
+        print("*** All tasks ***\n*** ***")
+        for task in db.tasks.find():
+            pprint.pprint(task)
+            print("------")
